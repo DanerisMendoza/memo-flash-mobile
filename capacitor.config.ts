0fe908ecf39e2bfc2a26d638e11c2edf003e469e
Daneris Mendoza <106788394+DanerisMendoza@users.noreply.github.com>
2024-05-23 09:43:05 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'memo-flash-mobile',
+  webDir: 'www'
+};
+
+export default config;
