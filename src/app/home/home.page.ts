34f69e12128d86475d274f415d0210a799604fd2
Daneris Mendoza <106788394+DanerisMendoza@users.noreply.github.com>
2024-05-23 12:04:15 +0800

    login ui initial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  username : string = ""
+  password : string = ""
+  constructor() { }
+  ngOnInit(): void {
+    console.log('mounted')
+  }
+  onLogin() {
+    const trimmedUsername = this.username.trim();
+    const trimmedPassword = this.password.trim();
 
-  constructor() {}
+    // Check if username or password is empty or just whitespace
+    if (!trimmedUsername || !trimmedPassword) {
+      alert('Please enter both username and password.');
+      return; // Exit the function if validation fails
+    }
 
+    console.log(this.username);
+    console.log(this.password);
+  }
 }
